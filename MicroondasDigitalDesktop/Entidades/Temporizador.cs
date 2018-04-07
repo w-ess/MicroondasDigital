922aad7a1c7a1d8e0322179371a445365cac8ba2
wesley_araujo <w-ess@hotmail.com>
2018-04-07 17:21:49 -0300

    Add project files.

---
+++ MicroondasDigitalDesktop/Entidades/Temporizador.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace MicroondasDigitalDesktop.Entidades
+{
+   public class Temporizador : Form
+    {
+        public int Horas { get; set; }
+        public int Minutos { get; set; }
+        public int Segundos { get; set; }
+
+
+        private static System.Timers.Timer aTimer;
+
+        // Construtor
+        public Temporizador()
+        {         
+            aTimer = new System.Timers.Timer();            
+            aTimer.Interval = 1000;
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+           // this.Segundos = 10;
+        }
+
+        // Iniciar
+        public void Inciar()
+        {
+            aTimer.Start();
+        }
+
+        // Pausa
+        public void Parar()
+        {
+            aTimer.Stop();
+        }
+
+
+        // Play e Pausa
+        public void Iniciar_Parar()
+        {          
+
+            if (aTimer.Enabled == false)
+            {
+                aTimer.Start();
+            }
+            else
+            {
+                aTimer.Stop();
+            }
+        }
+
+
+
+        // Evento do timer
+        public virtual void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+
+        }
+
+
+
+    }
+}
---
+++ MicroondasDigitalDesktop/View/MicroondasForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using MicroondasDigitalDesktop.Entidades;
+
+namespace MicroondasDigitalDesktop
+{
+    public partial class Form1 : Temporizador
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Microondas microondas = new Microondas();
+        
+
+        private void btnIniciaPausa_Click(object sender, EventArgs e)
+        {
+            microondas.Segundos = 5;
+            microondas.Iniciar_Parar();
+            //*** falta pegar o valor do evento do timer na class pai e pegar esse valor nesse form
+        }
+
+        public override void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            base.OnTimedEvent(source, e);
+
+            microondas.Segundos--;
+            label1.Invoke(new MethodInvoker(delegate { label1.Text = Convert.ToString(microondas.Segundos); }));
+            if (microondas.Segundos <= 0)
+            {
+                microondas.Parar();
+            }
+        }
+
+
+    }
+}
